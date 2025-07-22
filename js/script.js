c8aff69e018b94fd3c4e2de7ce2da10621181021
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-07-22 19:40:27 -0300

    Ajustando slides para Slick JS

--- js/script.js
+++ js/script.js
@@ -75,3 +75,41 @@ document.querySelector(".fa-linkedin").addEventListener("click", function () {
   window.open("https://linkedin.com", "_blank");
 
 });
+
+$('.responsive').slick({
+  dots: true,
+  infinite: true,
+  speed: 500,
+  autoplay: false,
+  slidesToShow: 4,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 769,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+		
