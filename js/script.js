4ff0b07aee8ad590699741b1f5cd4e57c35608a9
Filipe <92062371+Filipe-Alexandre@users.noreply.github.com>
2025-07-22 21:34:32 -0300

    Ajustando tablet landscape

--- js/script.js
+++ js/script.js
@@ -84,8 +84,17 @@ $('.responsive').slick({
   slidesToShow: 4,
   slidesToScroll: 4,
   responsive: [
+        {
+      breakpoint: 1400,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
     {
-      breakpoint: 1024,
+      breakpoint: 1025,
       settings: {
         slidesToShow: 3,
         slidesToScroll: 3,
